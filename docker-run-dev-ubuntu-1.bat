6e5d999c0a4db7f69b9eebfeb2fe3976a807ecfe
Nathan Quach <nathanquach8x@gmail.com>
2019-08-09 16:29:57 +0000

    map port

--- docker-run-dev-ubuntu-1.bat
+++ docker-run-dev-ubuntu-1.bat
@@ -3,4 +3,5 @@ docker run --rm -it ^
 -v "C:\Users\Nathan Quach\.ssh":/root/.ssh ^
 -v "C:\Users\Nathan Quach\projects\.gitconfig":/etc/gitconfig ^
 -w /root/projects/ ^
+-p 3000:3000 ^
 dev-ubuntu-1
