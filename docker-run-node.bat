d322dc3c9edfad7161084dcd6557d363049a8531
Nathan Quach <nathanquach8x@gmail.com>
2019-08-15 20:20:30 +1000

    update docker's ports

--- docker-run-node.bat
+++ docker-run-node.bat
@@ -2,5 +2,7 @@ docker run --rm -it ^
 -v "C:\Users\Nathan Quach\projects":/root/projects ^
 -v "C:\Users\Nathan Quach\projects\.gitconfig":/etc/gitconfig ^
 -p 3000:3000 ^
+-p 8080:8080 ^
+-p 9000:9000 ^
 -w /root/projects/ ^
 quachn/node-10
