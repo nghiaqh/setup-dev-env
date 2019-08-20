cb182500982603b80365fe5431f4b623078cd341
Nathan Quach <nathanquach8x@gmail.com>
2019-08-21 00:02:33 +1000

    Setup volumes for yarn_cache

--- run-node.bat
+++ run-node.bat
@@ -1,6 +1,8 @@
 docker run --rm -it ^
 -v "C:\Users\Nathan Quach\projects":/root/projects ^
 -v "C:\Users\Nathan Quach\projects\.gitconfig":/etc/gitconfig ^
+-v "C:\Users\Nathan Quach\projects\.yarn_cache":/yarn_cache ^
+-v "C:\Users\Nathan Quach\projects\.npm_cache":/npm_cache ^
 -p 3000:3000 ^
 -p 8080:8080 ^
 -p 9000:9000 ^
